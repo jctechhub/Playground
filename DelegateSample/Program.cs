05c604e61cea6f02004069a7cff67e74441090a3
jctechhub <jcdsta@gmail.com>
2019-05-30 10:18:09 +1000

    Delegate sample: basic, and Multicasting.

--- DelegateSample/Program.cs
+++ DelegateSample/Program.cs
@@ -4,9 +4,28 @@ namespace DelegateSample
 {
     class Program
     {
+        delegate void Test(string name);
+
         static void Main(string[] args)
         {
+            Test t = new Test(Show);
+            t.Invoke("Bob");
+            t("John");
+
+            t += new Test(Display);  //Multi-casting: this will call 'Show', and then Call 'Display'. 
+            t += new Test(Display); // this will not trigger 'Show' call, only 'Display' call. 
+            t -= new Test(Display);  //remove the reference. 
+            t("Jenny");
+
             Console.WriteLine("Hello World!");
+        }
+
+        static void Show(string name){
+            Console.WriteLine("Show Hello " + name);
+        }
+
+        static void Display(string name){
+            Console.WriteLine("Display Hello " + name);
         }
     }
 }
