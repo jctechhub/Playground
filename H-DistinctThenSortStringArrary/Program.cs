98c89d71c9f9787ea6184cb6b48fc33cfcbce4d7
Jerry Chen <jcdsta@gmail.com>
2019-06-03 06:14:47 +1000

    H-DistinctSortString:
    - given a list of strings
    - distinct them
    - then sort in desc order

---
+++ H-DistinctThenSortStringArrary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var list = new List<string>() { 
+                "Alex",
+                "Michael",
+                "Harry",
+                "Dave",
+                "Michael",
+                "Victor",
+                "Harry",
+                "Alex",
+                "Mary",
+                "Mary"
+                };
+
+            Console.WriteLine(writeIn(list));
+
+        }
+
+        public static string writeIn(List<string> ballot)
+        {
+            if (ballot == null || ballot.Count == 0) return "";
+            
+            var maxNumber= ballot.GroupBy(x => x)
+                .Select(g => new {key = g.Key, Count = g.Count()})
+                .Max(m => m.Count);
+            var result = ballot.GroupBy(x => x)
+                .Select(g => new { key = g.Key, Count = g.Count() })
+                .Where(k=>k.Count == maxNumber)
+                .Select(x=>x.key)
+                .ToList();
+            result.Sort();
+            return result.Last();
+        }
+
+    }
+}
