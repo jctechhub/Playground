e3b02ca319093ab9ef21a7e08d7c5432b8e193cb
jctechhub <jcdsta@gmail.com>
2019-06-04 10:21:00 +1000

    Group test by 'Trait'-Category

--- Test-Xunit/xunit.calculatorTests/xunit.calculatorTests/CalculatorTests.cs
+++ Test-Xunit/xunit.calculatorTests/xunit.calculatorTests/CalculatorTests.cs
@@ -6,17 +6,37 @@ namespace xunit.calculatorTests
     public class CalculatorTests
     {
         [Fact]
-        public void test_add()
+        [Trait("CalCategory", "Add")]
+        public void test_add_success()
         {
             var cal = new Calculator();
             Assert.True(cal.Add(3, 4) == 7, "Cal Add result is 7. ");
         }
 
         [Fact]
-        public void test_subtract()
+        [Trait("CalCategory", "Add")]
+        public void test_add_fail()
+        {
+            var cal = new Calculator();
+            Assert.False(cal.Add(3, 4) == 10, "Cal Add result isn't 10. ");
+        }
+
+
+
+        [Fact]
+        [Trait("CalCategory", "Subtract")]
+        public void test_subtract_success()
         {
             var cal = new Calculator();
             Assert.True(cal.Subtract(3, 4) == -1, "Cal subtract result is -1. ");
+        }
+
+        [Fact]
+        [Trait("CalCategory", "Subtract")]
+        public void test_subtract_fail()
+        {
+            var cal = new Calculator();
+            Assert.False(cal.Subtract(3, 4) == -10, "Cal subtract result isnot  -10. ");
         }
     }
 }
