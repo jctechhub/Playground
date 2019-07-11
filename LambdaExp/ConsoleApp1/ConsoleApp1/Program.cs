1147e3554ac2389c523fa780db82185051af2e7c
jctechhub <jcdsta@gmail.com>
2019-07-11 10:10:24 +1000

    Func<> and Local function sample

---
+++ LambdaExp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sample func method
+            Func<int, int> Square = number => number * number;
+            Console.WriteLine(Square(5));
+
+            //same as Func<>, recommended by resharper
+            int Square1(int number) => number * number;
+            Console.WriteLine(Square1(5));
+            
+        }
+
+
+
+    }
+}
