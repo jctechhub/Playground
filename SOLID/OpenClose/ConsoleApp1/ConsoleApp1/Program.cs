e76009fc8bbe0739838ca3fa359d4d652a031958
Jerry Chen <jcdsta@gmail.com>
2019-07-27 19:48:50 +1000

    Open/Close principle - bad case violations

--- SOLID/OpenClose/ConsoleApp1/ConsoleApp1/Program.cs
+++ SOLID/OpenClose/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,14 +9,51 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //var apple =new Prodct("Apple")
+            var apple = new Product("Apple", Color.Green, Size.small);
+            var tree = new Product("tree", Color.Green, Size.Large);
+            var house = new Product("house", Color.blue, Size.Large);
 
+            Product[] products = {apple, tree, house};
 
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (OLD):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
             Console.WriteLine("Hello World!");
         }
 
 
     }
+
+
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> items, Size size)
+        {
+            foreach (var p in items)
+            {
+                if (p.Size == size)
+                    yield return p;
+            }
+        }
+
+        ///PROBLEM: need to add more method in this class to extend the filter functionality. 
+        /// it violates the Open/Close principle: open for extension and close for Modifications.  
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> items, Color color)
+        {
+            foreach (var p in items)
+            {
+                if (p.Color == color)
+                    yield return p;
+            }
+        }
+        
+    }
+
+
 
     public enum Color { Red, Green, blue }
     public enum Size { small, Medium, Large}
@@ -35,20 +72,6 @@ namespace ConsoleApp1
         }
     }
 
-  
-
-    public class ProductFilter
-    {
-        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> items, Size size)
-        {
-            foreach (var p in items)
-            {
-                if (p.Size == size)
-                    yield return p;
-            }
-        }
-    }
-    
         
 
 }
