9797b4743d942edb9784e4632c546ae1859e3a68
jctechhub <jcdsta@gmail.com>
2019-07-27 13:02:32 +1000

    Add in SOLID example, first OpenClose

---
+++ SOLID/OpenClose/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var apple =new Prodct("Apple")
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+
+    public enum Color { Red, Green, blue }
+    public enum Size { small, Medium, Large}
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+  
+
+    public class ProductFilter
+    {
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> items, Size size)
+        {
+            foreach (var p in items)
+            {
+                if (p.Size == size)
+                    yield return p;
+            }
+        }
+    }
+    
+        
+
+}
