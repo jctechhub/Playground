015567e592d7ecb922979f8eefe171499d055033
jctechhub <jcdsta@gmail.com>
2019-07-29 10:16:49 +1000

    Faceted Builder

---
+++ DesignPattern/Builder/FacatedBuilder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Works
+                .At("Microsoft")
+                .AsA("Software Developer")
+                .Earning(100000)
+                .Lives
+                .At("101 George street")
+                .In("Sydney")
+                .WithPostcode("2000");
+
+            Console.WriteLine(person);
+
+            Console.Read();        
+        }
+    }
+
+    public class Person
+    {
+        public string StreetAddress, Postcode, City;
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+
+    public class PersonBuilder
+    {
+        protected Person person = new Person(); //must be 'Protected' for inherited classes. 
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        /// <summary>
+        /// returns the Person that has been built. if without, call Person would just return the type of the person being built. 
+        /// </summary>
+        /// <param name="pb"></param>
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;  //this allows the chaining of subsequent calls. 
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int annualIncome)
+        {
+            person.AnnualIncome = annualIncome;
+            return this;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        // might not work with a value type!
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+
+    }
+}
+
+
