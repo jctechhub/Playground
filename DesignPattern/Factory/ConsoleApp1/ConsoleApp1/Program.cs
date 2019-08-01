2bb4a4464d2f90b6e28da0fc51985a10a829ca9d
jctechhub <jcdsta@gmail.com>
2019-08-02 09:17:19 +1000

    Factory method: Bad way of creating object

---
+++ DesignPattern/Factory/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point(1.0, 2.0, CoordinateSystem.Cartesian);
+            Console.WriteLine($"normal way: {p}");
+        }
+    }
+
+
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+
+    public class Point
+    {
+        private double x, y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        /// <summary>
+        /// Bad way of creating object, by passing in enum to determine what type. 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="cs"></param>
+        public Point(double a,
+            double b, // names do not communicate intent
+            CoordinateSystem cs = CoordinateSystem.Cartesian)
+        {
+            switch (cs)
+            {
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    x = a;
+                    y = b;
+                    break;
+            }
+
+            // steps to add a new system
+            // 1. augment CoordinateSystem
+            // 2. change ctor
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+
+}
