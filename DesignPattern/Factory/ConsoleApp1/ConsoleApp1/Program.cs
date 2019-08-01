20a55b5eb5fa5720f9a4bdb025cc598b69fb0ef8
jctechhub <jcdsta@gmail.com>
2019-08-02 09:36:19 +1000

    Factory method: different ways of fixing the problems

--- DesignPattern/Factory/ConsoleApp1/ConsoleApp1/Program.cs
+++ DesignPattern/Factory/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp1
 {
@@ -6,8 +7,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var p = new Point(1.0, 2.0, CoordinateSystem.Cartesian);
+            var p = new Point(1.0, 2.0, CoordinateSystem.Cartesian); //bad way: by creating objects like this, we need to bind the enum to the constructors. 
             Console.WriteLine($"normal way: {p}");
+
+            var p1 = Point.NewCartesianPoint(2, 3);
+            Console.WriteLine($"factory method 1: {p1}");
+
+            var p2 = Point.Factory.NewCartesianPoint(3, 4);
+            Console.WriteLine($"factory method 2: {p2}");
+
+            var p3 = PointFactory.NewCartesianPoint(4, 5);
+            Console.WriteLine($"factory method 3: {p3}");
+
         }
     }
 
@@ -27,6 +38,32 @@ namespace ConsoleApp1
             this.x = x;
             this.y = y;
         }
+
+
+
+        // factory method
+
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            //new implementation of Cartesian point
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            //new implementation of Polar point
+            return null;
+        }
+
+        // make it lazy, this is similar to .NET method: Task.Factory.xxx
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+        }
+
         /// <summary>
         /// Bad way of creating object, by passing in enum to determine what type. 
         /// </summary>
@@ -60,5 +97,11 @@ namespace ConsoleApp1
         }
     }
 
-
+    class PointFactory
+    {
+        public static Point NewCartesianPoint(float x, float y)
+        {
+            return new Point(x, y); // needs to be public
+        }
+    }
 }
