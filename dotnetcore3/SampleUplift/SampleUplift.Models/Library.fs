27c1d22dc6d9e80c4bcfcfa38e514c9f8c3befcc
jctechhub <jcdsta@gmail.com>
2019-10-21 13:45:14 +1100

    Add RuntimeCompilation in order for html refresh in Dev time

---
+++ dotnetcore3/SampleUplift/SampleUplift.Models/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace SampleUplift.Models
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ dotnetcore3/SampleUplift/SampleUplift.Utilities/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace SampleUplift.Utilities
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
