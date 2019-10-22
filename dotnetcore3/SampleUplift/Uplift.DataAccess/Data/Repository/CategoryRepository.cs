71b33b2a0f25d8272be7887716c901744fb9906f
jctechhub <jcdsta@gmail.com>
2019-10-23 09:40:17 +1100

    (Milestone) Adding CategoryRepo and UnitOfWOrk

--- dotnetcore3/SampleUplift/Uplift.DataAccess/Data/Repository/CategoryRepository.cs
+++ dotnetcore3/SampleUplift/Uplift.DataAccess/Data/Repository/CategoryRepository.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
 
 namespace Uplift.DataAccess.Data.Repository
 {
-    public class CategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Category category)
+        {
+            var objectFromDB = _db.Category.FirstOrDefault(s => s.Id == category.Id);
+            objectFromDB.Name = category.Name;
+            objectFromDB.DisplayOrder = category.DisplayOrder;
+            _db.SaveChanges();
+        }
     }
 }
