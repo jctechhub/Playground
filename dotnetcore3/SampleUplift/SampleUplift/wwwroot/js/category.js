b211c5ec75a0409149e9d3cff5ce21113e31c253
jctechhub <jcdsta@gmail.com>
2019-11-01 20:59:13 +1100

    Controller update

--- dotnetcore3/SampleUplift/SampleUplift/wwwroot/js/category.js
+++ dotnetcore3/SampleUplift/SampleUplift/wwwroot/js/category.js
@@ -23,8 +23,8 @@ function loadDataTable() {
                                     <i class='far fa-edit'></i> Edit
                                 </a>
 &nbsp;
-                                <a href="/Admin/category/Delete/${data}" class='btn btn-success text-white' style='cursor:pointer; width: 100px;'>
-                                    <i class='far fa-edit'></i> Delete
+                                <a href="/Admin/category/Delete/${data}" class='btn btn-danger text-white' style='cursor:pointer; width: 100px;'>
+                                    <i class='far fa-trash-alt'></i> Delete
                                 </a>
 
 
@@ -40,3 +40,29 @@ function loadDataTable() {
     });
 }
 
+function Delete(url) {
+    swal({
+        title: "are you sure you want to delete?", 
+        text: "you will not be able to restore the content", 
+        type: "warning", 
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55", 
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: true
+    }, function() {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function(data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+
+    );
+}
