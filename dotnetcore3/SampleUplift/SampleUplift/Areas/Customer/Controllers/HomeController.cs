defa9e0f77ce7741caf6588b7b1bc723c6cf996f
Jerry Chen <jcdsta@gmail.com>
2019-11-08 22:42:53 +1100

    [MS] SessionExtension for Dotnetcore3

--- dotnetcore3/SampleUplift/SampleUplift/Areas/Customer/Controllers/HomeController.cs
+++ dotnetcore3/SampleUplift/SampleUplift/Areas/Customer/Controllers/HomeController.cs
@@ -36,6 +36,13 @@ namespace SampleUplift.Controllers
             return View(HomeVM);
         }
 
+        public IActionResult Details(int id)
+        {
+            var serviceFromDb = _unitOfWork.Service.GetFirstOrDefault(includeProperties: "Category,Frequency", filter: c => c.Id == id);
+            return View(serviceFromDb);
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
---
+++ dotnetcore3/SampleUplift/SampleUplift/Extensions/SessionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace SampleUplift.Extensions
+{
+    /// <summary>
+    /// Note: sample template for session. DOTNETCore3 doesn't deal with objects excepts string in session. needs custom implementations. 
+    /// </summary>
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
