9593ae85441d96cfd5772499bee6516efbd9c355
Jerry Chen <jcdsta@gmail.com>
2019-11-08 11:05:40 +1100

    [MS] Adding Service controller

---
+++ dotnetcore3/SampleUplift/SampleUplift/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace SampleUplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]  //NOTE: this defines the route
+    [Authorize]
+
+    public class ServiceController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment; //this is from DI, for image upload 
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API Calls
+        public IActionResult GetAll()
+        {
+            //NOTE: IncludeProperties: use eager loading to populate object in binding. 
+            return Json(new { data = _unitOfWork.Service.GetAll(includeProperties: "Category,Frequency") });
+        }
+
+        //[HttpDelete]
+        //public IActionResult Delete(int id)
+        //{
+        //    var serviceFromDb = _unitOfWork.Service.Get(id);
+        //    string webRootPath = _hostEnvironment.WebRootPath;
+        //    var imagePath = Path.Combine(webRootPath, serviceFromDb.ImageUrl.TrimStart('\\'));
+        //    if (System.IO.File.Exists(imagePath))
+        //    {
+        //        System.IO.File.Delete(imagePath);
+        //    }
+
+        //    if (serviceFromDb == null)
+        //    {
+        //        return Json(new { success = false, message = "Error while deleting." });
+        //    }
+
+        //    _unitOfWork.Service.Remove(serviceFromDb);
+        //    _unitOfWork.Save();
+        //    return Json(new { success = true, message = "Deleted Successfully." });
+        //}
+
+        #endregion
+
+
+    }
+}
---
+++ dotnetcore3/SampleUplift/Uplift.Models/ViewModels/ServiceVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Uplift.Models.ViewModels
+{
+    public class ServiceVM
+    {
+        public Service Service { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> FrequencyList { get; set; }
+    }
+}
