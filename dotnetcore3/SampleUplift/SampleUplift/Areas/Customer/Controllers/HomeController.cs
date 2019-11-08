b0a656e1d517e9b148b238632b66c033acb4a0b0
Jerry Chen <jcdsta@gmail.com>
2019-11-08 22:32:43 +1100

    adding home view

--- dotnetcore3/SampleUplift/SampleUplift/Areas/Customer/Controllers/HomeController.cs
+++ dotnetcore3/SampleUplift/SampleUplift/Areas/Customer/Controllers/HomeController.cs
@@ -5,23 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Uplift.DataAccess.Data.Repository.IRepository;
 using Uplift.Models;
+using Uplift.Models.ViewModels;
 
 namespace SampleUplift.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private HomeViewModel HomeVM;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM = new HomeViewModel()
+            {
+                CategoryList = _unitOfWork.Category.GetAll(),
+                ServiceList = _unitOfWork.Service.GetAll(includeProperties: "Frequency")
+            };
+
+            return View(HomeVM);
         }
 
         public IActionResult Privacy()
