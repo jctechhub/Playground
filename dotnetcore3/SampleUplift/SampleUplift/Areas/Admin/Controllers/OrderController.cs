021eda09f2fdf35f9f3081dd6b6868dea6298b2c
jctechhub <jcdsta@gmail.com>
2019-11-13 10:23:14 +1100

    add orders

--- dotnetcore3/SampleUplift/SampleUplift/Areas/Admin/Controllers/OrderController.cs
+++ dotnetcore3/SampleUplift/SampleUplift/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models.ViewModels;
 using Uplift.Utility;
 
 namespace SampleUplift.Areas.Admin.Controllers
@@ -24,37 +25,37 @@ namespace SampleUplift.Areas.Admin.Controllers
             return View();
         }
 
-        //public IActionResult Details(int id)
-        //{
-        //    OrderViewModel orderVM = new OrderViewModel()
-        //    {
-        //        OrderHeader = _unitOfWork.OrderHeader.Get(id),
-        //        OrderDetails = _unitOfWork.OrderDetails.GetAll(filter: o => o.OrderHeaderId == id)
-        //    };
-        //    return View(orderVM);
-        //}
+        public IActionResult Details(int id)
+        {
+            OrderViewModel orderVM = new OrderViewModel()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(id),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(filter: o => o.OrderHeaderId == id)
+            };
+            return View(orderVM);
+        }
 
-        //public IActionResult Approve(int id)
-        //{
-        //    var orderFromDb = _unitOfWork.OrderHeader.Get(id);
-        //    if (orderFromDb == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    _unitOfWork.OrderHeader.ChangeOrderStatus(id, SD.StatusApproved);
-        //    return View(nameof(Index));
-        //}
+        public IActionResult Approve(int id)
+        {
+            var orderFromDb = _unitOfWork.OrderHeader.Get(id);
+            if (orderFromDb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.OrderHeader.ChangeOrderStatus(id, SD.StatusApproved);
+            return View(nameof(Index));
+        }
 
-        //public IActionResult Reject(int id)
-        //{
-        //    var orderFromDb = _unitOfWork.OrderHeader.Get(id);
-        //    if (orderFromDb == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    _unitOfWork.OrderHeader.ChangeOrderStatus(id, SD.StatusRejected);
-        //    return View(nameof(Index));
-        //}
+        public IActionResult Reject(int id)
+        {
+            var orderFromDb = _unitOfWork.OrderHeader.Get(id);
+            if (orderFromDb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.OrderHeader.ChangeOrderStatus(id, SD.StatusRejected);
+            return View(nameof(Index));
+        }
 
 
 
