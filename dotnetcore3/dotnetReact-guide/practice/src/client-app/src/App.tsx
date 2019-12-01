9c7e71bd082ff70a80bfbae9a48be00f281e97c3
jctechhub <jcdsta@gmail.com>
2019-12-02 10:00:44 +1100

    [MS] Add sematic UI React for styling:
    - go to 'https://react.semantic-ui.com/'
    - run 'npm install sematic-ui-react'
    - refer to the documentations

--- dotnetcore3/dotnetReact-guide/practice/src/client-app/src/App.tsx
+++ dotnetcore3/dotnetReact-guide/practice/src/client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';  //NOTE: need to import '{Component}'
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react'
 
 class App extends Component {  //NOTE: Compoent declaration. 
   state={
@@ -11,20 +11,22 @@ class App extends Component {  //NOTE: Compoent declaration.
     axios.get('http://localhost:5000/api/values').then(response => {
       this.setState({
         values: response.data
-      });
+      }); 
     });   
   }
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {this.state.values.map((value: any) => (  //NOTE: MUST declare value to :any type, otherwise compilation error. 
-            <li key={value.id}>{value.name}</li>
+      <div>
+       <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {this.state.values.map((value: any) => (  //NOTE: MUST declare value to :any type, otherwise compilation error. 
+            <List.Item key={value.id}> {value.name}</List.Item>
           ))}
-        </ul>
-      </header>
+      </List>      
+      
     </div>   
     );  
   }
