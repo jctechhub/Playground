67808662776bdae010c92c02282137fe5c148bc5
Jerry Chen <jcdsta@gmail.com>
2020-07-17 09:50:19 +1000

    adding in Post method from test

---
+++ APITestings/RSDemo1/RSDemo1/Model/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSDemo1.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
--- APITestings/RSDemo1/RSDemo1/UnitTest1.cs
+++ APITestings/RSDemo1/RSDemo1/UnitTest1.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using RestSharp;
 using RestSharp.Serialization.Json;
+using RSDemo1.Model;
 using Utf8Json;
 
 namespace RSDemo1
@@ -34,5 +35,49 @@ namespace RSDemo1
             Assert.That(obs["author"].ToString(), Is.EqualTo("Karthik KK"), "Author is not correct");
 
         }
+
+
+
+        [Test]
+        public void PostWithAnonymousBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new { name = "Raj" });
+            request.AddUrlSegment("postid", 1);
+
+            var response = client.Execute(request);
+
+            //1. use jsonDeserializer 
+            var des = new JsonDeserializer();
+            var output = des.Deserialize<Dictionary<string, string>>(response);
+            var result = output["name"];
+            Assert.That(result, Is.EqualTo("Raj"), "Author is correct");
+
+
+
+        }
+
+        [Test]
+        public void PostWithBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Post(){Author = "Kenny", Id = 19, Title = "hello world" });
+            var response = client.Execute(request);
+
+            //1. use jsonDeserializer 
+            var des = new JsonDeserializer();
+            var output = des.Deserialize<Dictionary<string, string>>(response);
+            var result = output["Author"];
+            Assert.That(result, Is.EqualTo("Kenny"), "Author is correct");
+
+
+
+        }
+
+
     }
 }
